73906bba5c640041473c6740fc3594fd14da8d0c
Prasad <prasadpatange2603@gmail.com>
2022-08-04 17:31:48 +0530

    node-mon

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-// const color = require('colors')
-// console.log("Package.json".blue);
-// console.log("Package.json".rainbow);
-
-import colors from 'colors';
-console.log(colors.blue("Package.json"));
-console.log(colors.rainbow("Package.json"));
-
-// import statement for latest versinof nodejs
-// in package.json =>
-// {
-//     "type": "module",
-// }
-// then use below code same for colors
-
-import chalk from 'chalk';
-console.log(chalk.blue('Hello world!'))
-console.log(chalk.underline.bgYellow('Hello world!'))
+console.log("Try Nodemon with nodejs...");
+console.log(10+20);
