f225ab8e7f9e5c2f36e8c634aff112b6139f310f
Prasad <prasadpatange2603@gmail.com>
2022-08-05 11:54:58 +0530

    basic Api

---
+++ data.js
@@ -0,0 +1,7 @@
+const data = [
+    {name:'prasad',email:'prasad@gmail.com'},
+    {name:'ram',email:'ram@gmail.com'},
+    {name:'shyam',email:'shyam@gmail.com'},
+    {name:'ganesh',email:'ganesh@gmail.com'}
+]
+module.exports = data;
--- index.js
+++ index.js
@@ -1,2 +1,11 @@
-console.log("Try Nodemon with nodejs...");
-console.log(10+20);
+const http = require('http');
+const data = require('./data');
+const PORT = 4500;
+
+http.createServer((req,res) =>{
+    res.writeHead(200,{'Content-Type':'application\json'});
+    res.write(JSON.stringify(data))
+    res.end();
+}).listen(PORT,()=>{
+    console.log(`Server Running at http://localhost:${PORT}/`);
+});
