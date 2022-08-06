7d48fdbbf3ff4df34bf2acc4e749c6bc9b70965c
Prasad <prasadpatange2603@gmail.com>
2022-08-06 12:33:54 +0530

    crud operation with file system

--- index.js
+++ index.js
@@ -1,18 +1,33 @@
-const fs = require('fs');
-// fs.writeFileSync("apple.txt","This is fruit")
-const path = require('path');
-const dirPath = path.join(__dirname,'files');//give file path 
-console.log(dirPath);
+const fs = require("fs");
+const path = require("path");
+const dirPath = path.join(__dirname, "crud");
+const filePath = `${dirPath}/apple.txt`;
+//CREATE
+fs.writeFileSync(filePath, "This is Simple Text File");
 
-// create multiple files
-// for(let i=0; i<5; i++){
-//     fs.writeFileSync(`${dirPath}/hello${i}.txt`,"This is Simple File.")
-// }
+//READ
+// fs.readFile(filePath,(err,item) =>{
+//     console.log(item);
+//     //output : <Buffer 54 68 69 73 20 69 73 20 53 69 6d 70 6c 65 20 54 65 78 74 20 46 69 6c 65>
+// })
+fs.readFile(filePath, "utf-8", (err, item) => {
+  console.log(item);
+  // This is Simple Text File
+});
 
-// Read file
-fs.readdir(dirPath,(err,files) =>{
-    // console.log(files);//array form
-    files.forEach((item) =>{
-        console.log("File Name Is : ",item);//single file
-    })
-})
+// UPDATE
+fs.appendFile(filePath, " and file name is apple.txt", (err) => {
+  if (!err) console.log("File Updated.");
+});
+
+// RENAME
+fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
+  if (!err) console.log("File Name is Updated.");
+});
+
+// DELETE
+fs.unlinkSync(`${dirPath}/fruit.txt`);
+
+// Interview Question :
+// 1) What is Buffer :
+//  -> It is Temporary Memory Location ,nodejs use this for performing operations.
