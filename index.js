13a6c19389d71229bfcac45d0fda847181dc54ee
Prasad <prasadpatange2603@gmail.com>
2022-08-06 21:11:48 +0530

    Handle Async Data using Promise

--- index.js
+++ index.js
@@ -1,20 +1,12 @@
-// Asynchronous Programming/Operation :
-// In This ,second task do not wait to finish first task.
-console.log("Start Execution...");
-
-setTimeout(() =>{
-    console.log("Logic Execution...");
-},2000)
-
-console.log("Complete Execution...");
-
-// Draback of Async Programming :
-// not update value.
-let a = 10;
+// Handle Async Data using Promise in Nodejs :
+let a = 20;
 let b = 0;
-
-setTimeout(()=>{
-    b = 20;
-},2000);
-
-console.log(a+b);
+let waitingData = new Promise((resolve,reject) =>{
+    setTimeout(()=>{
+        resolve(30);
+    },2000);
+})
+waitingData.then((data) =>{
+    b = data;
+    console.log(a+b);
+})
