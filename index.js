80a85d7d5f28e23ab5779b3046c19f09ed555436
Prasad <prasadpatange2603@gmail.com>
2022-08-06 20:48:39 +0530

    Async Operation'

--- index.js
+++ index.js
@@ -1,33 +1,20 @@
-const fs = require("fs");
-const path = require("path");
-const dirPath = path.join(__dirname, "crud");
-const filePath = `${dirPath}/apple.txt`;
-//CREATE
-fs.writeFileSync(filePath, "This is Simple Text File");
+// Asynchronous Programming/Operation :
+// In This ,second task do not wait to finish first task.
+console.log("Start Execution...");
 
-//READ
-// fs.readFile(filePath,(err,item) =>{
-//     console.log(item);
-//     //output : <Buffer 54 68 69 73 20 69 73 20 53 69 6d 70 6c 65 20 54 65 78 74 20 46 69 6c 65>
-// })
-fs.readFile(filePath, "utf-8", (err, item) => {
-  console.log(item);
-  // This is Simple Text File
-});
+setTimeout(() =>{
+    console.log("Logic Execution...");
+},2000)
 
-// UPDATE
-fs.appendFile(filePath, " and file name is apple.txt", (err) => {
-  if (!err) console.log("File Updated.");
-});
+console.log("Complete Execution...");
 
-// RENAME
-fs.rename(filePath, `${dirPath}/fruit.txt`, (err) => {
-  if (!err) console.log("File Name is Updated.");
-});
+// Draback of Async Programming :
+// not update value.
+let a = 10;
+let b = 0;
 
-// DELETE
-fs.unlinkSync(`${dirPath}/fruit.txt`);
+setTimeout(()=>{
+    b = 20;
+},2000);
 
-// Interview Question :
-// 1) What is Buffer :
-//  -> It is Temporary Memory Location ,nodejs use this for performing operations.
+console.log(a+b);
