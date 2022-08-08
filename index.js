a7e3d5e9b5a98a4c96c3051ca9c98bff10bbff16
Prasad <prasadpatange2603@gmail.com>
2022-08-08 13:56:02 +0530

    template engine ejs

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-// Make HTML Pages in NodeJS :  
+//Template Engine ejs to make Dynamic Web Pages :  
 const express = require('express');
 const path = require('path');
 const publicPath = path.join(__dirname,"public");
@@ -6,28 +6,39 @@ console.log(__dirname);
 console.log(publicPath);
 // make express Executable
 const app = express();
-// use() expressJS function
-// app.use(express.static(publicPath));
+
+// set template Engine ejs
+app.set('view engine','ejs');
+
 
 app.get("",(_,res) =>{
   res.sendFile(`${publicPath}/index.html`);
-})
+});
+
+// ejs file
+app.get("/profile",(_,res) =>{
+  const user = {
+    name : "Prasad",
+    email : "Prasad@gmail.com"
+  }
+   res.render("profile",{user});
+});
 
 app.get("/aboutMe",(_,res) =>{
   res.sendFile(`${publicPath}/about.html`);
-})
+});
 
 app.get("/help",(_,res) =>{
   res.sendFile(`${publicPath}/help.html`);
-})
+});
 
 // Error Page
 app.get("*",(_,res) =>{
   res.sendFile(`${publicPath}/nopage.html`);
-})
+});
 
 
 app.listen(5000,()=>{
   console.log("Server Started...")
-})
+});
 
