f3777e1984f7adb58d30ec3c276454d7d9847c8a
Prasad <prasadpatange2603@gmail.com>
2022-08-08 12:26:50 +0530

    Render HTML Tags and JSON Data in NodeJS

--- index.js
+++ index.js
@@ -1,24 +1,38 @@
+// Render HTML Tags and JSON Data in NodeJS :  
 const express = require('express');
 // to make executable
 const app = express();
 
 app.get('',(req,res) =>{
-  // put in browser : http://localhost:5000/?name=prasad
-  // console.log("Data Sent By Browser => ",req.query);
-  // Object => Data Sent By Browser =>  { name: 'prasad' }
-  console.log("Data Sent By Browser => ",req.query.name);
-  // name => Data Sent By Browser =>  prasad
-  // res.send("Welcome Folks , This is Home Page");
-  res.send("Welcome , " + req.query.name);
-  // Welcome , prasad
+  res.send(`<h1>Welcome Folks , This is Home Page</h1>
+  <a href="/about" >Go to About Page</a>
+  `);
 });
 
 app.get('/about',(req,res) =>{
-  res.send("Hello , This is About Page");
+  // take data from queryParams : http://localhost:5000/about?name=prasad
+  res.send(`
+  <input type="text" placeholder="Enter Username" value="${req.query.name}" />
+  <button>Click Me</button>
+  <br/>
+  <a href= "/">Go to Home Page</a>
+  <br/>
+  <a href="/help" >Go to Help Page</a>
+  `);
 });
 
 app.get('/Help',(req,res) =>{
-  res.send("Hello , This is Help Page");
+  // JSON Data
+  res.send([
+    {
+      name : "Prasad",
+      email : "prasad@gmail.com"
+    },
+    {
+      name : "Pro",
+      email : "Pro@gmail.com"
+    }
+  ]);
 });
 
 app.listen(5000,()=>{
