63d463c00de9d6c9e806248e4a10c105ce11e049
Prasad <prasadpatange2603@gmail.com>
2022-08-09 21:20:08 +0530

    nodejs post api method

--- api.js
+++ api.js
@@ -1,14 +1,29 @@
-const express = require('express');
-const dbConnect = require('./mongodb');
+const express = require("express");
+const dbConnect = require("./mongodb");
 const app = express();
 
-app.get('/', async (req,res) =>{
-    let data = await dbConnect();
-        data = await data.find().toArray();
-        console.log(data);
-    res.send(data);
+// to get data from postman use middleware
+// before express v4.6 we use body parser but above version 
+// replace this by express.json()
+app.use(express.json());
+
+app.get("/", async (req, res) => {
+  let data = await dbConnect();
+  data = await data.find().toArray();
+  console.log(data);
+  res.send(data);
 });
 
-app.listen(5000,() =>{
-    console.log("server started at http://localhost:5000/")
+// store new data through API use post()
+app.post("/", async (req, res) => {
+  // console.log(req.body);
+  // res.send({name:'prasad'});
+  // res.send(req.body);
+  let data = await dbConnect();
+//   req.body => data comes from postman / angular / react etc.into nodejs
+  let result = await data.insertOne(req.body); //data comes from postman / angular / react etc.into nodejs
+  res.send(result);
+});
+app.listen(5000, () => {
+  console.log("server started at http://localhost:5000/");
 });
