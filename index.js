234cf10a411052fd6ea90d00b4748ac9a287f20b
Prasad <prasadpatange2603@gmail.com>
2022-08-09 16:04:31 +0530

    read data mongodb

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
-// const MongoClient   = require('mongodb').MongoClient;
-// or
-const { MongoClient } = require("mongodb");
-const url = "mongodb://localhost:27017";
-const client = new MongoClient(url);
-const database = "e-comm";
+// Make separate file for dbConnect
+const dbConnect = require('./mongodb');
 
-async function getData() {
-  // Use connect method to connect to the server
-  let result = await client.connect();
-  let db = result.db(database);
-  let collection = db.collection('products');
-  // Promise { <pending> } that's why
-  let response = await collection.find({}).toArray();
-  console.log(response);
+// const { MongoClient } = require("mongodb");
+// const url = "mongodb://localhost:27017";
+// const client = new MongoClient(url);
+// const database = "e-comm";
+
+// async function dbConnect() {
+//   // Use connect method to connect to the server
+//   let result = await client.connect();
+//   let db = result.db(database);
+//   return db.collection('products');
+// }
+
+// //dbConnect() return a  Promise { <pending> }
+
+// // 1) 1 ST METHOD TO HANDLE PROMISE :
+
+// dbConnect().then((res) =>{
+//   // console.log(res.find().toArray());
+//   // return a  Promise { <pending> }
+//   res.find().toArray().then((data)=>{
+//     console.log("All data : ",data);
+//   })
+//   // res.find({name:"M 11"}).toArray().then((data)=>{
+//   //   console.log("Single data : ",data);
+//   // })
+// })
+// // console.log(dbConnect());
+
+// // 2) 2 ND METHOD TO HANDLE PROMISE : better ways to this morden way
+const main = async () =>{
+  let data = await dbConnect();
+      // data = await data.find({name:'M 11'}).toArray();
+      data = await data.find().toArray();
+      console.log(data);
 }
 
-getData();
+main();
