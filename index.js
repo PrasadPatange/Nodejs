ca18a3b3c8e38730ad7d9a66e3f62e8ba4787457
Prasad <prasadpatange2603@gmail.com>
2022-08-10 16:09:46 +0530

    curd with API in mongoose

--- index.js
+++ index.js
@@ -1,60 +1,54 @@
-// Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports both promises and callbacks.
-const mongoose = require("mongoose");
+const express = require("express");
+require("./config");
+const Product = require("./products");
 
-mongoose.connect('mongodb://localhost:27017/e-comm');
-const ProductSchema = new mongoose.Schema({
-    name: String,
-    price: Number,
-    brand: String,
-    category: String
+// make app executable
+const app = express();
+// parse string data comming from postman into JSON
+app.use(express.json());
 
+// POST API
+// http://localhost:5000/create
+app.post("/create", async(req,res) =>{
+    let data = new Product(req.body);
+    let result = await data.save();
+    // console.log(req.body);
+    console.log(result);
+    // res.send("Done");
+    res.send(result);
 });
 
-// Save data in DB
-const saveInDB = async () => {
-  const ProductModel = mongoose.model("products", ProductSchema);
-  let data = new ProductModel({
-    name: "motorolla",
-    price: 500,
-    brand: "moto",
-    category: "mobile",
-  });
-  let result = await data.save(data);
-  console.log(result);
-};
+// GET API
+//http://localhost:5000/list
+app.get('/list',async(req,res) =>{
+    let data = await Product.find();
+    console.log(data);
+    res.send(data);
+});
 
-// saveInDB(); //you want to save data just comment out
+// DELETE API
+// http://localhost:5000/delete/62f21a0e838fe9bd03b60ea1
+app.delete('/delete/:_id',async(req,res) =>{
+    console.log(req.params);
+    let data = await Product.deleteOne(req.params);
+    res.send(data);
+});
 
-// Update data in DB
-const updateInDB = async () =>{
-    const ProductModel = mongoose.model("products", ProductSchema);
-    let data = await ProductModel.updateOne(
-        {name:"motorolla"},
+// PUT API
+// http://localhost:5000/update/62f2209a57b3e0c346f10021
+app.put('/update/:_id',async(req,res) =>{
+    console.log(req.params);
+    let data = await Product.updateOne(
+        // {} condition
+        req.params,
+        // data to be update
         {
-            $set:{price:750,name:"motoG"}
+            $set :req.body
         }
-    );
-    console.log(data);
-}
+        );
+    res.send(data);
+});
 
-// updateInDB();  //you want to update data just comment out
-
-// Delete data in DB
-const deleteDB = async () =>{
-    const ProductModel = mongoose.model("products", ProductSchema);
-    let data = await ProductModel.deleteOne({name:"motoG"});
-    console.log(data);
-}
-
-// deleteDB();
-
-
-// Find or Search data in DB
-const findInDB = async () =>{
-    const ProductModel = mongoose.model("products", ProductSchema);
-    // let data = await ProductModel.find();
-    let data = await ProductModel.find({ name: 'samsung J7'});
-    console.log(data);
-}
-
-findInDB();
+app.listen(5000,()=>{
+    console.log("server started...")
+})
