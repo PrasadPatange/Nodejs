03d46a7ddf64c3547c2e3456b3a0a6029bede20c
Prasad <prasadpatange2603@gmail.com>
2022-08-10 15:11:48 +0530

    curd with mongoose

--- index.js
+++ index.js
@@ -1,16 +1,60 @@
 // Mongoose is a MongoDB object modeling tool designed to work in an asynchronous environment. Mongoose supports both promises and callbacks.
 const mongoose = require("mongoose");
 
-const main = async () => {
-    await mongoose.connect("mongodb://localhost:27017/e-comm");
-    const ProductSchema = new mongoose.Schema({
-        name:String,
-        price:Number
-    });
-    const ProductModel = mongoose.model('products',ProductSchema);
-    let data = new ProductModel({name:"m8",price:1000})
-    let result = await data.save(data);
-    console.log(result);
+mongoose.connect('mongodb://localhost:27017/e-comm');
+const ProductSchema = new mongoose.Schema({
+    name: String,
+    price: Number,
+    brand: String,
+    category: String
+
+});
+
+// Save data in DB
+const saveInDB = async () => {
+  const ProductModel = mongoose.model("products", ProductSchema);
+  let data = new ProductModel({
+    name: "motorolla",
+    price: 500,
+    brand: "moto",
+    category: "mobile",
+  });
+  let result = await data.save(data);
+  console.log(result);
+};
+
+// saveInDB(); //you want to save data just comment out
+
+// Update data in DB
+const updateInDB = async () =>{
+    const ProductModel = mongoose.model("products", ProductSchema);
+    let data = await ProductModel.updateOne(
+        {name:"motorolla"},
+        {
+            $set:{price:750,name:"motoG"}
+        }
+    );
+    console.log(data);
 }
 
-main();
+// updateInDB();  //you want to update data just comment out
+
+// Delete data in DB
+const deleteDB = async () =>{
+    const ProductModel = mongoose.model("products", ProductSchema);
+    let data = await ProductModel.deleteOne({name:"motoG"});
+    console.log(data);
+}
+
+// deleteDB();
+
+
+// Find or Search data in DB
+const findInDB = async () =>{
+    const ProductModel = mongoose.model("products", ProductSchema);
+    // let data = await ProductModel.find();
+    let data = await ProductModel.find({ name: 'samsung J7'});
+    console.log(data);
+}
+
+findInDB();
