30f3308a0d59c87a208b0efdaa16c0f1bad02725
Prasad <prasadpatange2603@gmail.com>
2022-08-10 13:09:02 +0530

    nodejs delete api method

--- api1.js
+++ api1.js
@@ -1,14 +1,11 @@
 const express = require("express");
 const dbConnect = require("./mongodb");
-// to delete data using id require Object id
 const mongodb = require('mongodb');
 const app = express();
 
-// to get data from postman use middleware
-// before express v4.6 we use body parser but above version 
-// replace this by express.json()
 app.use(express.json());
 
+// get API
 app.get("/", async (req, res) => {
   let data = await dbConnect();
   data = await data.find().toArray();
@@ -25,9 +22,7 @@ app.post("/", async (req, res) => {
 
 // put() use to update data with API
 app.put('/',async (req,res) =>{
-    // console.log(req.body);
     let data = await dbConnect();
-    // pass dynamic data from postman : 
     let result = data.updateOne(
         {name:req.body.name},
         {
@@ -39,12 +34,10 @@ app.put('/',async (req,res) =>{
 });
 
 // Delete Method : pass id in params to delete data
-// http://localhost:5000/62f21a01838fe9bd03b60ea0
 app.delete('/:id',async (req,res) =>{
     console.log(req.params.id);
     const data = await dbConnect();
     const result = await data.deleteOne({_id:new mongodb.ObjectId(req.params.id)})
-    // res.send("Done");
     res.send(result);
 })
 
