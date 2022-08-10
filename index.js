3d45c82ca57112a1efd79c3eeec41fabcffff5c9
Prasad <prasadpatange2603@gmail.com>
2022-08-10 17:09:18 +0530

    file upload in nodejs

--- index.js
+++ index.js
@@ -1,25 +1,33 @@
-const express = require("express");
-require("./config");
-const Product = require("./products");
+// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. It is written on top of busboy for maximum efficiency.
+const { application } = require('express');
+const express = require('express');
+const multer = require('multer');
 
 const app = express();
 
-app.use(express.json());
+// Date for filename :
+// let date = new Date();
+// let current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
 
-app.get('/search/:key', async(req,res) =>{
-    console.log(req.params.key)
-    let data = await Product.find(
-        {
-            "$or" : [
-                { "name" : {$regex:req.params.key}},
-                { "brand" : {$regex:req.params.key}},
-                { "category" : {$regex:req.params.key}}
-            ]
+// create function for upload => work as a middleware 
+
+const upload = multer({
+    storage: multer.diskStorage({
+        destination: function (req, file, cb) {
+            // cb = call back
+            cb(null,"uploads");
+        },
+        filename: function (req, file, cb){
+            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
         }
-    );
-    res.send(data);
-})
+    })
+}).single("user_file")
+
+// http://localhost:5000/upload
+app.post('/upload',upload,(req,res) =>{
+    res.send("file Upload.");
+});
 
 app.listen(5000,()=>{
-    console.log("server started...")
-})
+    console.log("Server Started...")
+});
