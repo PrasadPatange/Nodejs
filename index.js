04622051349ebfec022d744fad103dfffa96af9a
Prasad <prasadpatange2603@gmail.com>
2022-08-12 14:36:30 +0530

    mysql GET API in Nodejs

--- index.js
+++ index.js
@@ -1,22 +1,17 @@
-const mysql = require("mysql");
+const express = require("express");
+const con = require("./config");
+const app = express();
 
-const con = mysql.createConnection({
-    host :'localhost',
-    user : "root",
-    password : "",
-    database :"demo"
+app.get("/", (req, res) => {
+  // res.send("Route Done");
+  con.query("select * from users", (err, result) => {
+    if (err) {
+      res.send("Error");
+    } else {
+      res.send(result);
+      console.log(result);
+    }
+  });
 });
 
-//This is Only for Connection Check. 
-// con.connect((err) =>{
-//     if(err){
-//         console.log("Error");
-//     }else{
-//         console.log("Connected");
-//     }
-// });
-
-// Get Data From mysql Database
-con.query("select * from note",(err,result) =>{
-    console.log("Result : ",result);
-})
+app.listen(5000);
