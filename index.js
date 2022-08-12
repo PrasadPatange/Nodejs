9d574b7779be724b5ef7a312df17884a6366d446
Prasad <prasadpatange2603@gmail.com>
2022-08-12 12:01:43 +0530

    EventEmitter in Nodejs

--- index.js
+++ index.js
@@ -1,8 +1,34 @@
-const os = require('os');
-console.log(os);
-console.log(os.arch());
-console.log(os.freemem()/(1024*1024*1024)); //free space
-console.log(os.totalmem()/(1024*1024*1024)); //total space
-console.log(os.hostname()); //DESKTOP-PRASAD
-console.log(os.platform()); //win32
-console.log(os.userInfo()); //user name and so on...
+const express = require('express');
+const EventEmitter = require('events'); //in-build module. EventEmitter is a class that's why we use 1st letter capital.
+const app = express();
+// create event Object
+const event = new EventEmitter();
+// Handle 
+let count = 0;
+event.on("countAPI",()=>{
+    // capture event
+    count++;
+    console.log("event Called",count);
+})
+
+
+app.get("/",(req,res)=>{
+    res.send("Api Called");
+    // Generate Event
+    // emit event
+    event.emit("countAPI");
+});
+
+app.get("/search",(req,res)=>{
+    res.send("Search Api Called");
+    event.emit("countAPI");
+});
+
+app.get("/update",(req,res)=>{
+    res.send("Update Api Called");
+    event.emit("countAPI");
+});
+
+app.listen(5000,()=>{
+    console.log("Server Started...");
+})
