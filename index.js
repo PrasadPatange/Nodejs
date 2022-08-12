72b42ff6c8ac29e4733d1ebb374627a50e12e7a8
Prasad <prasadpatange2603@gmail.com>
2022-08-12 11:24:12 +0530

    os Module

--- index.js
+++ index.js
@@ -1,32 +1,8 @@
-// Multer is a node.js middleware for handling multipart/form-data, which is primarily used for uploading files. It is written on top of busboy for maximum efficiency.
-const express = require('express');
-const multer = require('multer');
-
-const app = express();
-
-// Date for filename :
-// let date = new Date();
-// let current_date = date.getFullYear()+"-"+(date.getMonth()+1)+"-"+ date.getDate();
-
-// create function for upload => work as a middleware 
-
-const upload = multer({
-    storage: multer.diskStorage({
-        destination: function (req, file, cb) {
-            // cb = call back
-            cb(null,"uploads");
-        },
-        filename: function (req, file, cb){
-            cb(null, file.fieldname + "-" + Date.now() + ".jpg")
-        }
-    })
-}).single("user_file")
-
-// http://localhost:5000/upload
-app.post('/upload',upload,(req,res) =>{
-    res.send("file Upload.");
-});
-
-app.listen(5000,()=>{
-    console.log("Server Started...")
-});
+const os = require('os');
+console.log(os);
+console.log(os.arch());
+console.log(os.freemem()/(1024*1024*1024)); //free space
+console.log(os.totalmem()/(1024*1024*1024)); //total space
+console.log(os.hostname()); //DESKTOP-PRASAD
+console.log(os.platform()); //win32
+console.log(os.userInfo()); //user name and so on...
